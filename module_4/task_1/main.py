4a1e0cffdddaa06abdf98130ea246ff8a8f24c6a
Valentyna <valentino4ka2009@gmail.com>
2024-02-05 10:56:04 +0200

    task_1 - Error is handled

--- module_4/task_1/main.py
+++ module_4/task_1/main.py
@@ -2,9 +2,12 @@ from salary import total_salary
 
 
 def main():
-    total, average = total_salary("module_4/task_1/salary/salaries.txt")
-    print(f"Загальна сума заробітної плати: {total},"
-          f"Середня заробітна плата: {average}")
+    if total_salary("module_4/task_1/salary/salaries.txt") == "File not found":
+        print("File not found")
+    else:
+        total, average = total_salary("module_4/task_1/salary/salaries.txt")
+        print(f"Загальна сума заробітної плати: {total},"
+              f"Середня заробітна плата: {average}")
 
 
 if __name__ == "__main__":
